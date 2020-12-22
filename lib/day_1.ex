69916c290821afc92722e7573116ac70a5855f60
Evren Kutar <evren.kutar@gmail.com>
2020-12-22 15:56:21 +0100

    mix format files and add input file

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -14,6 +14,7 @@ defmodule Day1 do
   """
   def checkFor2020([h | t]) do
     l = 2020 - h
+
     if Enum.member?(t, l) do
       h * l
     else
@@ -23,37 +24,45 @@ defmodule Day1 do
 
   defmodule M do
     def c([head | tail], leftover) do
-      IO.inspect binding()
+      # stdout variables
+      # IO.inspect binding()
       cond do
-        Enum.member?(tail, (leftover - head)) ->
-          {head, (leftover-head)}
-        Enum.member?(tail, (head - leftover)) ->
-          {head, (head-leftover)}
+        Enum.member?(tail, leftover - head) ->
+          {head, leftover - head}
+
+        Enum.member?(tail, head - leftover) ->
+          {head, head - leftover}
+
         true ->
           c(tail, leftover)
       end
     end
-    def c([], leftover) do
+
+    def c([], _leftover) do
       {0, 0}
     end
   end
 
   def check3([h | t]) do
     l = 2020 - h
-    IO.inspect binding()
+    # stdout variables
+    # IO.inspect binding()
     r = M.c(t, l)
-    if elem(r,0) + elem(r,1) + h == 2020 do
-      h*elem(r,0)*elem(r,1)
+
+    if elem(r, 0) + elem(r, 1) + h == 2020 do
+      h * elem(r, 0) * elem(r, 1)
     else
       check3(t)
     end
   end
 
   def processFile do
-    {:ok, fileInput} = File.read("input.txt")
-    s = for n <- String.split(fileInput, "\n"), do: unless n == "", do: String.to_integer(n)
-    sNoNil = Enum.filter(s, & !is_nil(&1))
-    # checkFor2020(s)
+    {:ok, fileInput} = File.read("inputs/day1.txt")
+    s = for n <- String.split(fileInput, "\n"), do: unless(n == "", do: String.to_integer(n))
+    sNoNil = Enum.filter(s, &(!is_nil(&1)))
+    # solution for first question
+    checkFor2020(sNoNil)
+    # solution for second question
     check3(sNoNil)
   end
 end
